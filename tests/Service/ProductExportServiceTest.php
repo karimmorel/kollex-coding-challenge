647ae520a57fe29b449e9745fde45c516f6472a2
Karim <karim-morel@outlook.fr>
2020-09-21 12:27:07 +0200

    Working case test

--- tests/Service/ProductExportServiceTest.php
+++ tests/Service/ProductExportServiceTest.php
@@ -16,4 +16,55 @@ class ProductExportServiceTest extends TestCase {
         $this->assertEquals($data, '[]');
 
     }
+
+    // Testing when everything is going the right way
+    public function testWorkingCase()
+    {
+        $mockedExporter = $this->getMockBuilder(\kollex\Exporter\InternalFileExporter::class)
+        ->setMethods(['exportSource'])
+        ->setConstructorArgs(array('wholesaler_a.csv'))
+        ->getMock();
+
+        $returnedArray = array(
+            0 => array(
+                'id',
+                'ean',
+                'manufacturer',
+                'product',
+                'description',
+                'packaging product',
+                'foo',
+                'packaging unit',
+                'amount per unit',
+                'items on stock',
+                'warehouse'
+            ),
+            1 => array(
+                '18219821092',
+                '309828029082908',
+                'Drink Corp.',
+                'Soda Drink, 12 * 1,01',
+                'Lorem ipsum....',
+                'case 12',
+                'bar',
+                'bottle',
+                '1.0l',
+                '123',
+                'north'
+            )
+        );
+ 
+        $mockedExporter->method('exportSource')->willReturn($returnedArray);
+
+        $mapper = new \kollex\Mapper\WholesalerAMapper;
+        $provider = new \kollex\DataProvider\Assortment\JSONDataProvider;
+        $service = new \kollex\Service\ProductExportService($mockedExporter, $mapper, $provider);
+
+        $data = $service->export()->display();
+
+        // If the components are working the right way, I am expecting this result.
+        $testedData = '[{"id":"18219821092","gtin":"309828029082908","manufacturer":"Drink Corp.","name":"Soda Drink, 12 * 1,01","baseProductPackaging":"BO","baseProductAmount":1,"baseProductUnit":"LT","packaging":"CA","baseProductQuantity":12}]';
+
+        $this->assertEquals($data, $testedData);
+    }
 }
